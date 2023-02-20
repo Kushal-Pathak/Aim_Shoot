eb152964f287af17047b5d769210d08042389d3c
Kushal Pathak <kushalpathak80@gmail.com>
2023-02-20 16:02:22 +0545

    almost done

--- App.cpp
+++ App.cpp
@@ -30,16 +30,25 @@ public:
 		buffer[y][x] = '/'; //left leg
 		buffer[y][x + 2] = '\\'; //right leg
 	}
+	void draw(int x, int y) {
+		this->x = x; this->y = y;
+		if (y < 2) y = 2;
+		if (y > h - 1) y = h - 1;
+		if (x < 0) x = 0;
+		if (x > w - 3) x = w - 3;
+		buffer[y - 2][x + 1] = 'O'; // head
+		buffer[y - 1][x] = '/'; // left hand
+		buffer[y - 1][x + 1] = 'I'; // chest
+		buffer[y - 1][x + 2] = '\\'; // right hand
+		buffer[y][x] = '/'; //left leg
+		buffer[y][x + 2] = '\\'; //right leg
+	}
 	player() {
 
 	}
-	int hit(float x, float y) {
-		if (y == this->y - 2 && x == this->x + 1) return 1;
-		if (y == this->y - 1 && x == this->x) return 1;
-		if (y == this->y - 1 && x == this->x + 1) return 1;
-		if (y == this->y - 1 && x == this->x + 2) return 1;
-		if (y == this->y && x == this->x) return 1;
-		if (y == this->y && x == this->x + 2) return 1;
+	int hit(float bx, float by) {
+		if (bx >= x - 1 && bx <= x + 3 && by >= y - 3 && by <= y) return 1;
+		if (by == y - 3 && (bx == x || bx == x + 1 || bx == x + 2)) return 1;
 		return 0;
 	}
 };
@@ -78,6 +87,7 @@ int main() {
 	player p1(0, h,1), p2(w, h, 2);
 	projectile bullet;
 	turn = p1;
+	opponent = p2;
 	//game loop
 	while (!game_over) {
 		display_scene();
@@ -87,17 +97,22 @@ int main() {
 		bullet.set(vx, vy, turn);
 		int result = 0;
 		do {
-			display_scene();
 			bullet.travel();
-			Sleep(300);
+			display_scene();
+			Sleep(100);
 			result = opponent.hit(bullet.x, bullet.y);
 			if (result) {
+				bullet.travel();
 				game_over = 1;
 				display_scene();
 				cout << "Player-" << turn.id << " wins!";
 			}
 		} while (bullet.in_air() && !result);
+		cin.get(); cin.get();
 		change_turn(p1, p2);
+		init_buffer();
+		p1.draw(0, h);
+		p2.draw(w, h);
 	}
 
 	cin.get();
@@ -120,4 +135,6 @@ void init_buffer() {
 void change_turn(player p1, player p2) {
 	if (turn.id == 1) turn = p2;
 	else turn = p1;
+	if (opponent.id == 1) opponent = p2;
+	else opponent = p1;
 }
