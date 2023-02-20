a6e344ed651eb270eb8758763e46e159d06c4d15
Kushal Pathak <kushalpathak80@gmail.com>
2023-02-20 15:19:41 +0545

    improved projectile trajectory and input format

--- projectile_test.cpp
+++ projectile_test.cpp
@@ -3,7 +3,7 @@
 #include <windows.h>
 #define h 28
 #define w 115
-#define g 0.2f
+#define g 1
 using namespace std;
 
 char buffer[h][w];
@@ -12,7 +12,7 @@ int game_over = 0;
 
 void display_scene();
 void init_buffer();
-
+void change_turn();
 
 class player {
 public:
@@ -33,7 +33,7 @@ public:
 	player() {
 
 	}
-	int hit(float x, float y) {
+	int hit(int x, int y) {
 		if (y == this->y - 2 && x == this->x + 1) return 1;
 		if (y == this->y - 1 && x == this->x) return 1;
 		if (y == this->y - 1 && x == this->x + 1) return 1;
@@ -47,57 +47,50 @@ public:
 class projectile {
 public:
 	float x = 0, y = 0, vx = 0, vy = 0;
-	void set(float vel_x, float vel_y, player player) {
+	void set(float angle, float mag, player player) {
 		x = player.x + 2.0f + 1.0f; y = player.y - 1.0f - 2.0f;
-		vx = vel_x; vy = -vel_y;
+		vx = cos(angle) * mag;
+		vy = -sin(angle) * mag;
+
 		if (player.id == 2) {
-			x = player.x - 3.0f; y = player.y - 1.0f - 2.0f;
+			x = player.x - 1.0f; y = player.y - 1.0f - 2.0f;
 			vx = -vx;
 		}
 	}
 	int in_air() {
-		if (y < h - 1) return 1;
-		return 0;
+		if (this->y >= h - 1) return 0;
+		return 1;
 	}
 	void travel() {
-		int xx = (int)round(x), yy = (int)round(y);
-		if (xx >= 0 && xx < w && yy >= 0 && yy < h) {
-			buffer[yy][xx] = '*';
-		}
-		x += vx;
-		y += vy;
+		buffer[(int)round(y)][(int)round(x)] = '*';
+		x += 1;
+		y += 1;
 		vy += g;
 	}
 };
 
-player turn, opponent;
-void change_turn(player, player);
+player turn;
 
 int main() {
 	init_buffer();
-	player p1(0, h,1), p2(w, h, 2);
+	player p1(0, h, 1), p2(w, h, 2);
 	projectile bullet;
-	turn = p1;
+	turn = p2;
 	//game loop
 	while (!game_over) {
 		display_scene();
 		cout << "Player-" << turn.id << "'s turn: ";
-		float vx, vy;
-		cin >> vx >> vy;
-		bullet.set(vx, vy, turn);
-		int result = 0;
+		float angle, mag;
+		cin >> angle >> mag;
+		bullet.set(angle, mag, turn);
+		int i = 0;
 		do {
 			display_scene();
+			cout << i;
+			i++;
 			bullet.travel();
-			Sleep(300);
-			result = opponent.hit(bullet.x, bullet.y);
-			if (result) {
-				game_over = 1;
-				display_scene();
-				cout << "Player-" << turn.id << " wins!";
-			}
-		} while (bullet.in_air() && !result);
-		change_turn(p1, p2);
+			Sleep(500);
+		} while (bullet.in_air());
 	}
 
 	cin.get();
@@ -113,11 +106,11 @@ void display_scene() {
 }
 
 void init_buffer() {
-	for (int i = 0; i < h; i++) 
+	for (int i = 0; i < h; i++)
 		for (int j = 0; j < w; j++) buffer[i][j] = ' ';
 }
 
-void change_turn(player p1, player p2) {
-	if (turn.id == 1) turn = p2;
-	else turn = p1;
+void change_turn() {
+	if (turn.id == 1) turn.id = 2;
+	else turn.id = 1;
 }
